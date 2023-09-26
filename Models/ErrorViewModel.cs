9b21ee99228bdff7e1ff3f08d6912b4f9652e6e3
AGOSTINA BOTTARINI <47866440@ort>
2023-09-26 09:36:35 -0300

    Creando Proyecto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP09_Bottarini_Piva.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
