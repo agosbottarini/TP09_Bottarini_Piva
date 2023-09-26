736181e50664e0cc9ebb0e79450ac40f2ed97510
AGOSTINA BOTTARINI <47866440@ort>
2023-09-26 10:20:14 -0300

    controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,6 +22,20 @@ public class HomeController : Controller
     {
         return View();
     }
+        public IActionResult Olvide()
+    {
+        return View();
+    }
+
+       public IActionResult Bienvenida()
+    {
+        return View();
+    }
+
+       public IActionResult Registro()
+    {
+        return View();
+    }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
