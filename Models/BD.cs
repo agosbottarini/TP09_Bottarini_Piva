b6c9248e0378ff4b135730543798c892f74c147e
AGOSTINA BOTTARINI <47866440@ort>
2023-09-28 08:14:45 -0300

    basta

--- Models/BD.cs
+++ Models/BD.cs
@@ -4,32 +4,32 @@ public static class BD
 {
     public static string _connectionString = @"localost";
 
-    public static Usuario ObtenerUsuario(string Username, string Contraseña)
+    //public static Usuario ObtenerUsuario(string Username, string Contraseña)
     {
-       Usuario UsuarioEncontrado = null;
-       using(SqlConnection db = new SqlConnection(_connectionString))
+       //Usuario UsuarioEncontrado = null;
+       //using(SqlConnection db = new SqlConnection(_connectionString))
        {
-            string sql = "SELECT * FROM Usuario WHERE username = @pUsername AND contraseña = @pContraseña";
-            UsuarioEncontrado = db.QueryFirstOrDefault<Usuario>(sql, new {pUsername = username, PContraseña = contraseña});
+          //  string sql = "SELECT * FROM Usuario WHERE username = @pUsername AND contraseña = @pContraseña";
+           // UsuarioEncontrado = db.QueryFirstOrDefault<Usuario>(sql, new {pUsername = username, PContraseña = contraseña});
        }
-       return UsuarioEncontrado;
+      // return UsuarioEncontrado;
     }
 
-    public static void AgregarUsuario(string Username, string Contraseña, int Telefono, string Gmail, int Edad)
+    //public static void AgregarUsuario(string Username, string Contraseña, int Telefono, string Gmail, int Edad)
     {
-        string SQL = "INSERT INTO Jugadores(UserName, Contraseña, Telefono, Gmail, Edad) VALUES (@PUserName, @pContraseña, @pTelefono, @pGmail, @pEdad)";
-        using(SqlConnection db = new SqlConnection(_connectionString))
+        //string SQL = "INSERT INTO Jugadores(UserName, Contraseña, Telefono, Gmail, Edad) VALUES (@PUserName, @pContraseña, @pTelefono, @pGmail, @pEdad)";
+        //using(SqlConnection db = new SqlConnection(_connectionString))
         {
-            db.Execute(SQL, new {pUsername = Usua.UserName, pContraseña = Usua.Contraseña, pTelefono = Usua.Telefono, pGmail = Usua.Gmail, pEdad = Usua.Edad})
+           // db.Execute(SQL, new {pUsername = Usua.UserName, pContraseña = Usua.Contraseña, pTelefono = Usua.Telefono, pGmail = Usua.Gmail, pEdad = Usua.Edad});
         }
     }
 
-    public static void CambiarContraseña(string Contraseña, string UserName)
+    //public static void CambiarContraseña(string Contraseña, string UserName)
     {
-        using(SqlConnection db = new SqlConnection(_connectionString))
+        //using(SqlConnection db = new SqlConnection(_connectionString))
         {
-            string SQL = "UPDATE Usuario SET Contraseña = pcontraseña WHERE UserName = @pUserName";
-            db.Execute(sql, new {pUserName = UserName, pcontraseña = Contraseña});
+            //string SQL = "UPDATE Usuario SET Contraseña = pcontraseña WHERE UserName = @pUserName";
+            //db.Execute(sql, new {pUserName = UserName, pcontraseña = Contraseña});
         }
     }
 
