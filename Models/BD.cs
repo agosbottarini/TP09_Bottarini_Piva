6dfff1c18786e40ddbb75d020f4b6ff32484f868
JOAQUIN PIVA <47870976@ort>
2023-09-28 08:24:30 -0300

    terminando mas cosas

--- Models/BD.cs
+++ Models/BD.cs
@@ -4,7 +4,7 @@ public static class BD
 {
     public static string _connectionString = @"localost";
 
-    public static Usuario ObtenerUsuario(string Username, string Contraseña)
+    public static Usuario ObtenerUsuario(string username, string contraseña)
     {
        Usuario UsuarioEncontrado = null;
        using(SqlConnection db = new SqlConnection(_connectionString))
@@ -15,23 +15,39 @@ public static class BD
        return UsuarioEncontrado;
     }
 
-    public static void AgregarUsuario(string Username, string Contraseña, int Telefono, string Gmail, int Edad)
+    public static void AgregarUsuario(string UserName, string Contraseña, int Telefono, string Gmail, int Edad)
     {
         string SQL = "INSERT INTO Jugadores(UserName, Contraseña, Telefono, Gmail, Edad) VALUES (@PUserName, @pContraseña, @pTelefono, @pGmail, @pEdad)";
         using(SqlConnection db = new SqlConnection(_connectionString))
         {
-            db.Execute(SQL, new {pUsername = Usua.UserName, pContraseña = Usua.Contraseña, pTelefono = Usua.Telefono, pGmail = Usua.Gmail, pEdad = Usua.Edad})
+            db.Execute(SQL, new {pUsername = UserName, pContraseña = Contraseña, pTelefono = Telefono, pGmail = Gmail, pEdad = Edad});
         }
     }
 
+    public static string ObtenerContraseña(int telefono)
+    {
+        string ContraseñaEncontrado = null;
+        using(SqlConnection db = new SqlConnection(_connectionString))
+        {
+            string sql = "SELECT Contraseña FROM Usuario WHERE telefono = @pTelefono";
+            ContraseñaEncontrado = db.QueryFirstOrDefault<string>(sql, new {pTelefono = telefono});
+        }
+        return ContraseñaEncontrado;
+    }
+
+
+    /*
     public static void CambiarContraseña(string Contraseña, string UserName)
     {
         using(SqlConnection db = new SqlConnection(_connectionString))
         {
             string SQL = "UPDATE Usuario SET Contraseña = pcontraseña WHERE UserName = @pUserName";
-            db.Execute(sql, new {pUserName = UserName, pcontraseña = Contraseña});
+            db.Execute(SQL, new {pUserName = UserName, pcontraseña = Contraseña});
         }
     }
+    */
+
+
 
 
 }
